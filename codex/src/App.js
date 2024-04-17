c6ead9e81c7ec3722d9dee12a17fffe5137c3e8b
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-04-17 15:28:49 +0530

    react and tailwind initiated

---
+++ codex/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className=' mx-1 bg-slate-800 text-white'>Hello world</div>
+    </div>
+  );
+}
+
+export default App;
