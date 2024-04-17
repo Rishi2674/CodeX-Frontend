25bddeaf39481b73bdeca2d0d2e253be75c1c2eb
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-04-17 15:49:36 +0530

    minor fixes made

--- codex/src/App.js
+++ codex/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div className=' mx-1 bg-slate-800 text-white'>Hello world</div>
+     
     </div>
   );
 }
 
 export default App;
+ 
---
+++ codex/src/Pages/CreateContest.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateContest = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CreateContest
