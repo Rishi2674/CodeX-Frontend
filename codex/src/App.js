c5d91d198c230a9e2ecef235c94bc80c23d45825
Rishi Upadhyay <rupadhyay2674@gmail.com>
2024-04-18 19:12:30 +0530

    create admin page done

--- codex/src/App.js
+++ codex/src/App.js
@@ -1,18 +1,34 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import CreateContest from "./pages/CreateContest";
 import DisplayQues from "./pages/DisplayQues";
 import Submissions from "./pages/Submissions";
 import SubmitQues from "./pages/SubmitQues";
+=======
+import './App.css';
+import Contest_Admin from './Pages/Contest_Admin';
+import CreateContest from './Pages/CreateContest';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       {/* <CreateContest /> */}
       {/* <DisplayQues /> */}
       {/* <SubmitQues /> */}
       <Submissions />
+=======
+     {/* <CreateContest/> */}
+     <Contest_Admin/>
+>>>>>>> Stashed changes
     </div>
   );
 }
 
 export default App;
+<<<<<<< Updated upstream
+=======
+
+ 
+>>>>>>> Stashed changes
